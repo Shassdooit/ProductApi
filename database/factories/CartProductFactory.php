c3de2d4fcb7cc5bb9723cc766df30c53223158ff
nicolaychudakov <nicolaychudakov@gmail.com>
2024-07-17 18:00:10 +0300

    use cases pravki + upgrade many

---
+++ database/factories/CartProductFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Cart;
+use App\Models\CartProduct;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class CartProductFactory extends Factory
+{
+
+    public function definition(): array
+    {
+        return [
+            'cart_id' => Cart::all()->random()->id,
+            'product_id' => Product::all()->random()->id,
+            'quantity' => $this->faker->numberBetween(1, 5),
+        ];
+    }
+}
